8c33d06b0f7c67c93c0f5d7d7a5cfb7e49b776b1
Svitlana Vakulenko <svakule@ilps-h1.science.uva.nl>
2020-04-22 14:04:27 +0200

    chg: fix transformer weights

--- MPBert_sampler_model.py
+++ MPBert_sampler_model.py
@@ -12,36 +12,42 @@ import torch
 import torch.nn as nn
 from torch.nn import CrossEntropyLoss, MSELoss
 
-from transformers.modeling_bert import BertPreTrainedModel, BertModel
+from transformers.modeling_distilbert import DistilBertPreTrainedModel, DistilBertModel
 from hdt import HDTDocument
 
 from settings import *
-from utils import adj
+from utils import *
 
 
-def build_look_up(keys, values):
+def build_look_up(keys):
     look_up_table = {}
     for i, k in enumerate(keys):
-        look_up_table[k.item()] = values[i].item()
+        look_up_table[k.item()] = i
     return look_up_table
 
 
-def look_up(look_up_table, subset, size=None, default_value=0):
-    if not size:
-        size = len(subset)
-    # pads to size with 0s
-    s_values = torch.zeros(size, 1)
+def look_up(look_up_table, subset, scores):
+    values = []
+    default_value_idx = len(scores)
     for i, key in enumerate(subset):
         if key in look_up_table:
             value = look_up_table[key]
         else:
-            value = default_value
-        s_values[[i]] = value
-    return s_values
+            # fill missing values with default
+            value = default_value_idx
+        values.append(value)
+    assert len(values) == len(subset)
+    positions = torch.tensor(values).unsqueeze(-1)
+    
+    # add 0 for entities that were not scored
+    s_scores = torch.zeros(size=(scores.shape[0]+1, 1))
+    s_scores[:scores.shape[0]] = scores
+
+    return s_scores.gather(0, positions)
 
 
 class SamplingLayer(nn.Module):
-    def __init__(self, kg, topk_entities=5):
+    def __init__(self, kg, topk_entities):
         super(SamplingLayer, self).__init__()
         self.kg = kg
         self.ksample = topk_entities
@@ -55,37 +61,33 @@ class SamplingLayer(nn.Module):
         Outputs:
             *subgraph*: subgraph edges and entities
         '''
-        
-        # get the top-k (predicates/)entities based on the score vectors
-        weights, indices = torch.sort(e_scores, descending=True)
-        top_scores = weights[:self.ksample]
-        top_entities = entity_ids[indices[:self.ksample]]  # choose top-k matching entities
-        sampled_entities = [e.item() for e in top_entities]
-        
-        
-        # build a lookup table for entity scores
-        e_table = build_look_up(top_entities, top_scores)
-        sampled_entities = list(e_table.keys())
-        
-        # build a lookup table for predicate scores
-        p_table = build_look_up(predicate_ids, p_scores)
+#         with torch.autograd.detect_anomaly():
+            # get the top-k (predicates/)entities based on the score vectors
+        weights, indices = torch.sort(e_scores.view(-1), descending=True)
+        sampled_entities = entity_ids[indices[:self.ksample]].tolist()  # choose top-k matching entities
+#         print("Retrieving adjacencies for %d entities"%len(sampled_entities))
 
-        # ? map the selected subset from the score vector indices to predicate/entity global ids
-        # sampled_predicates
+        # build a lookup table for entity & predicate scores
+        e_table = build_look_up(entity_ids)
+        p_table = build_look_up(predicate_ids)
+
+        # sample predicates?
+        sampled_predicates = []  # predicate_ids.tolist()
         
         # request kg through hdt api for a subgraph given entity and relation subsets
-        sampled_predicates = []  # consider all predicates without subsampling -> bigger subgraph retrieved
         self.kg.configure_hops(1, sampled_predicates, 'predef-wikidata2018-09-all', True, False)
-        s_entity_ids, s_predicate_ids, adjacencies = self.kg.compute_hops(sampled_entities, 50000000, 0)
+        s_entity_ids, s_predicate_ids, adjacencies = self.kg.compute_hops(sampled_entities, 50000, 0)
         
+        print("Retrieved new subgraph with %d entities and %d relations"%(len(s_entity_ids), len(s_predicate_ids)))
+
         # load subgraph into tensor
         indices, relation_mask = adj(adjacencies, len(s_entity_ids), len(s_predicate_ids))
+
+        # lookup local scores to activate respective entities & predicates
+        e_scores = look_up(e_table, s_entity_ids, e_scores)
+        p_scores = look_up(p_table, s_predicate_ids, p_scores)
         
-        # lookup local entity scores to activate entities
-        e_scores = look_up(e_table, s_entity_ids)
-        
-        # lookup local predicate scores to activate predicates
-        p_scores = look_up(p_table, s_predicate_ids)
+        del adjacencies, e_table, p_table, s_predicate_ids
         
         return (indices, e_scores, p_scores, relation_mask), s_entity_ids
 
@@ -102,9 +104,10 @@ class MPLayer(nn.Module):
             *y*: answer activations
         '''
         indices, e_scores, p_scores, relation_mask = subgraph
+        
         num_entities = e_scores.shape[0]
         num_relations = p_scores.shape[0]
-#         e_scores = e_scores.view(-1)
+#         relation_mask = relation_mask.unsqueeze(-1)
         p_scores = p_scores.view(-1)
         
         # propagate score from the Transformer output over predicate labels to predicate ids
@@ -115,6 +118,8 @@ class MPLayer(nn.Module):
         
         # MP step: propagates entity activations to the adjacent nodes
         y = torch.sparse.mm(subgraph.t(), e_scores)
+        
+        del subgraph, indices, e_scores, p_scores, 
 
         # and MP to entities summing over all relations
         y = y.view(num_relations, num_entities).sum(dim=0) # new dim for the relations
@@ -122,7 +127,7 @@ class MPLayer(nn.Module):
         return y
 
 
-class MessagePassingHDTBert(BertPreTrainedModel):
+class MessagePassingHDTBert(DistilBertPreTrainedModel):
     r"""
         **labels**: (`optional`) ``torch.LongTensor`` of shape ``(batch_size,)``:
             Labels for computing the sequence classification/regression loss.
@@ -150,33 +155,23 @@ class MessagePassingHDTBert(BertPreTrainedModel):
         loss, logits = outputs[:2]
     """
 
-    def __init__(self, config, num_entities, num_relations, weight=None, mp_layer=True, hdt_file='wikidata2018_09_11.hdt'):
+    def __init__(self, config, hdt_file='wikidata2018_09_11.hdt', topk_entities=10, bottleneck_dim=32):
         super(MessagePassingHDTBert, self).__init__(config)
-        self.mp_layer = mp_layer
-        if self.mp_layer:
-            # the output layer is the distribution over answer-entities
-            self.num_labels = num_entities
-        else:
-            # the output layer is the distribution over relation labels
-            self.num_labels = config.num_labels
         
         # entity matching Transformer
-        self.bert = BertModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
+        self.bert = DistilBertModel(config)
+        self.dropout = nn.Dropout(config.dropout)
+        self.pre_classifier = nn.Linear(config.hidden_size, bottleneck_dim)
+        self.classifier = nn.Linear(bottleneck_dim, self.config.num_labels)
         
         # initialise connection to the Wikidata KG through the HDT API
-        
-        # sampling layer with subgraph retrieval
         kg = HDTDocument(hdt_path+hdt_file)
-        self.subgraph_sampling = SamplingLayer(kg)
+        # sampling layer with subgraph retrieval
+        self.subgraph_sampling = SamplingLayer(kg, topk_entities)
         
-        # the predicted scores are then propagated via a message-passing layer into the entity subser distribution defined by the subgraph
+        # predicted scores are propagated via MP layer into the entity subser distribution defined by the subgraph
         self.mp = MPLayer()
         
-        # the output is a vector with the score distribution over the input entities
-        self.weight = weight
-
         self.init_weights()
 
     def forward(
@@ -185,54 +180,68 @@ class MessagePassingHDTBert(BertPreTrainedModel):
         predicates_q=None,
         answer=None,
     ):
-        
-        # predict entity matches
-        input_ids, token_type_ids, attention_mask, entity_ids = entities_q
-        e_outputs = self.bert(
-            input_ids,
-            attention_mask=attention_mask,
-            token_type_ids=token_type_ids,
-            position_ids=None,  # token types or position ids for separating concatenated input strings?? TODO
-            head_mask=None,  # what's that? TODO
-        )
-        e_outputs = self.dropout(e_outputs[1])
+        with torch.no_grad():
+            # predict entity matches
+            input_ids, token_type_ids, attention_mask, entity_ids = entities_q
+            print("Ranking %d entities"%(len(input_ids)))
+            e_outputs = self.bert(
+                input_ids,
+                attention_mask=attention_mask
+            )
+            hidden_state = e_outputs[0]
+            e_outputs = hidden_state[:, 0]  # pooled output
+            
+            # predict predicate matches
+            input_ids2, token_type_ids2, attention_mask2, predicate_ids = predicates_q
+            print("Ranking %d predicates"%(len(input_ids2)))
+            p_outputs = self.bert(
+                input_ids2,
+                attention_mask=attention_mask2
+            )
+            hidden_state = p_outputs[0]
+            p_outputs = hidden_state[:, 0]  # pooled output
+            
+        e_outputs = self.dropout(e_outputs)
+        # introduced bottleneck before the classifier
+        e_outputs = self.pre_classifier(e_outputs)
         entity_logits = self.classifier(e_outputs)
+        del input_ids, token_type_ids, attention_mask, e_outputs
         
-        # predict predicate matches
-        input_ids2, token_type_ids2, attention_mask2, predicate_ids = predicates_q
-        p_outputs = self.bert(
-            input_ids2,
-            attention_mask=attention_mask2,
-            token_type_ids=token_type_ids2,
-            position_ids=None,
-            head_mask=None,
-        )
-        p_outputs = self.dropout(p_outputs[1])
+        
+        p_outputs = self.dropout(p_outputs)
+        # introduced bottleneck before the classifier
+        p_outputs = self.pre_classifier(p_outputs)
         predicate_logits = self.classifier(p_outputs)
+        del input_ids2, token_type_ids2, attention_mask2, p_outputs
+        
         
         # sampling layer: pick top-scored entities and relations, retrieve the subgraph and load into tensor
-        subgraph, entity_ids = self.subgraph_sampling(entity_logits.view(-1), entity_ids,
-                                                      predicate_logits.view(-1), predicate_ids)
-        
-        num_labels = len(entity_ids)
-        answer_idx = torch.tensor([entity_ids.index(answer.item())])
+        print("Start sampling")
+        subgraph, entity_ids = self.subgraph_sampling(entity_logits, entity_ids,
+                                                      predicate_logits, predicate_ids)
         
         # MP layer takes predicate scores and propagates them to the adjacent entities
         logits = self.mp(subgraph)
+        del subgraph
+        
+        # candidate answers are all entities in the subgraph
+        num_entities = len(entity_ids)
         
         outputs = (logits,)
 
         if answer is not None:
             # terminate with 0-loss if correct answer is not in the subgraph
-            answer_id = answer.item()    
+            answer_id = answer.item()
+            # print(answer_id)
             if answer_id not in entity_ids:
                 print("Wrong subgraph selected")
                 loss = torch.tensor(0)
             else:
-                loss_fct = CrossEntropyLoss(weight=self.weight)
-                print(answer_idx)
-                print(logits.view(-1, num_labels).shape)
-                loss = loss_fct(logits.view(-1, num_labels), answer_idx)
+                print("Correct subgraph selected")
+                answer_idx = torch.tensor([entity_ids.index(answer_id)])
+                del entity_ids
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, num_entities), answer_idx)
             outputs = (loss,) + outputs
 
         return outputs  # (loss), logits
